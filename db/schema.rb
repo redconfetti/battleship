4345b88c4051194ed13eadbd0da7f209b26cd10a
Jason Miller <jason@redconfetti.com>
2015-12-30 11:21:00 -0800

    Adds game model with boolean status. Adds player spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151223204314) do
+ActiveRecord::Schema.define(version: 20151230191757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.boolean  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "players", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
