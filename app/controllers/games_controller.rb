9b925ac7fbf7e0b727e1c83aab6a44a2ec2b6ad0
Jason Miller <jason@redconfetti.com>
2015-12-30 19:37:48 -0800

    Removes unnecessary global vars from controller methods

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,23 +2,23 @@ class GamesController < ApplicationController
   before_action :authenticate_player!, only: [:create]
 
   def index
-    @games = Game.all
+    games = Game.all
     respond_to do |format|
-      format.json { render json: @games }
+      format.json { render json: games }
     end
   end
 
   def pending
-    @pending_games = Game.pending
+    pending_games = Game.pending
     respond_to do |format|
-      format.json { render json: @pending_games }
+      format.json { render json: pending_games }
     end
   end
 
   def create
-    @new_game = Game.create
+    new_game = Game.create
     respond_to do |format|
-      format.json { render json: @new_game }
+      format.json { render json: new_game }
     end
   end
 end
