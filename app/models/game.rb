bf5e386207385cf886ac5ed184c654e4c8ac16a3
Jason Miller <jason@redconfetti.com>
2015-12-30 19:43:13 -0800

    Adds formatted and Unix timestamp start date to game JSON

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,10 @@
 class Game < ActiveRecord::Base
   scope :pending, -> { where(status: 'pending') }
+
+  def as_json(options = {})
+    super().merge({
+      'startDate' => self.created_at.strftime('%m/%d/%y %I:%M %p'),
+      'startDateUnixTimestamp' => self.created_at.to_i
+    })
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Game.create(created_at: '2015-01-26 04:15:32') }
+
+  describe '#as_json' do
+    it 'returns json representation' do
+      result = subject.as_json
+      expect(result).to be_an_instance_of Hash
+    end
+
+    it 'includes start date in json' do
+      result = subject.as_json
+      expect(result['startDate']).to eq '01/26/15 04:15 AM'
+    end
+
+    it 'includes start date timestamp in json' do
+      result = subject.as_json
+      expect(result['startDateUnixTimestamp']).to eq 1422245732
+    end
+  end
 end
