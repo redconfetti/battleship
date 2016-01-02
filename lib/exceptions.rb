6b3c08db3315cb23f9c3f7ef70be5a72c50d1580
Jason Miller <jason@redconfetti.com>
2016-01-01 22:32:56 -0800

    Adds reference to doc with status symbols

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -2,6 +2,8 @@ module Exceptions
   class ClientError < StandardError; end
   class ServerError < StandardError; end
 
+  # See status symbols at http://guides.rubyonrails.org/layouts_and_rendering.html#using-render
+
   class BadRequest < ClientError
     def status_symbol; :bad_request; end
   end
