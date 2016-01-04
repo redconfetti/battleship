a609d0e701e48068596237f4291e30356fa0bf38
Jason Miller <jason@redconfetti.com>
2016-01-03 20:43:08 -0800

    Adds winner and loser ids to Game

---
+++ db/migrate/20160104043511_add_winner_and_loser_to_game.rb
@@ -0,0 +1,6 @@
+class AddWinnerAndLoserToGame < ActiveRecord::Migration
+  def change
+    add_column :games, :winner_id, :integer
+    add_column :games, :loser_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102193829) do
+ActiveRecord::Schema.define(version: 20160104043511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20160102193829) do
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
     t.integer  "current_player_id"
+    t.integer  "winner_id"
+    t.integer  "loser_id"
   end
 
   create_table "player_game_states", force: :cascade do |t|
