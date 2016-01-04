f508fdcd93cac5be21c160fc39eb4ff45ab25c83
Jason Miller <jason@redconfetti.com>
2016-01-03 23:19:43 -0800

    Adds notices for when game ended prematurely. Also adds notice when waiting for second player

--- app/assets/javascripts/controllers/playController.js
+++ app/assets/javascripts/controllers/playController.js
@@ -42,7 +42,15 @@ angular.module('battleship').controller('PlayController', ['$http', '$routeParam
   };
 
   var gameActive = function() {
-    return ($scope.game && $scope.game.status != 'complete') ? true : false;
+    return ($scope.playerGameState && $scope.playerGameState.game && $scope.playerGameState.game.status !== 'complete') ? true : false;
+  };
+
+  $scope.enemyPlayerPresent = function() {
+    return ($scope.playerGameState && $scope.playerGameState.enemy && $scope.playerGameState.enemy.id) ? true : false;
+  };
+
+  $scope.gamePrematurelyClosed = function() {
+    return (!gameActive() && !$scope.isWinner() && !$scope.isLoser()) ? true : false;
   };
 
   $scope.awaitingOpponentShot = function() {
@@ -50,11 +58,11 @@ angular.module('battleship').controller('PlayController', ['$http', '$routeParam
   };
 
   $scope.playerControlsEnabled = function() {
-    return (isCurrentTurn() && gameActive()) ? true : false;
+    return (gameActive() && $scope.enemyPlayerPresent() && isCurrentTurn()) ? true : false;
   };
 
   $scope.isWinner = function() {
-    return ($scope.playerGameState && $scope.playerGameState.stats && $scope.playerGameState.stats.enemyRemaining < 1) ? true : false;
+    return ($scope.enemyPlayerPresent() && $scope.playerGameState && $scope.playerGameState.stats && $scope.playerGameState.stats.enemyRemaining < 1) ? true : false;
   };
 
   $scope.isLoser = function() {
